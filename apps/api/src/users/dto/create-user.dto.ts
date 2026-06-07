e94cce3c224acc7aff50b58d2edc01e3ab204642
Talha Ghaffar <119955882+talha-ghaffar-ch@users.noreply.github.com>
2026-06-07 15:59:25 +0500

    Add API request validation

--- apps/api/src/users/dto/create-user.dto.ts
+++ apps/api/src/users/dto/create-user.dto.ts
@@ -1,8 +1,26 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
   firstName: string;
+
+  @IsString()
+  @MinLength(2)
   lastName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsOptional()
+  @IsString()
   phone?: string;
+
+  @IsString()
+  @MinLength(8)
   passwordHash: string;
+
+  @IsOptional()
+  @IsString()
   farmerType?: string;
 }
